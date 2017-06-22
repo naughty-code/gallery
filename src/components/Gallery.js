292a55bc35da1c62a0824abf2d51e8c32da6f324
naughty-code <javier.murar@gmail.com>
2017-06-22 18:19:21 -0400

    added pseudo responsive behavior to gallery grid

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -44,7 +44,7 @@ const Gallery = ({ pages, isFetching, fetchPage}) => (
           <GalleryPage
             key={i}
             items={page.items}
-            rowNumber={5}
+            rowNumber={6}
           />
           )
         }
